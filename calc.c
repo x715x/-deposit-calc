3001998fad6d615546ea7a9b78ba2329c14af4c7
x715x <toki@ohlol.ru>
2017-03-10 14:31:32 +0700

    Vichislenie procents

--- calc.c
+++ calc.c
@@ -1,4 +1,36 @@
-#include <stdio.h>int main(){
+#include <stdio.h>
+int bankir(int a, int b){
+    int c;
+    if (a < 1000000){
+        if (b < 31){
+            c = a - a * 0.1;
+        };
+        if ((b > 30) && (b < 121)){
+            c = a + a * 0.02;
+        };
+        if ((b > 120) && (b < 241)){
+            c = a + a * 0.06;
+        };
+        if ((b > 240) && (b < 366)){
+            c= a + a * 0.12;
+        };
+    } else{
+        if (b < 31){
+            c = a - a * 0.1;
+        };
+        if ((b > 30) && (b < 121)){
+            c = a + a * 0.03;
+        };
+        if ((b > 120) && (b < 241)){
+            c = a + a * 0.08;
+        };
+        if ((b > 240) && (b < 366)){
+            c= a + a * 0.15;
+        };
+    };
+    return c;
+};        
+int main(){
     int sum, srok, itog;
     printf("Введите сумму вклада: ");
     scanf("%d", &sum);
